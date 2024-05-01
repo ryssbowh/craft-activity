2fe456b920d8c42327d126e5f6e50dbce241d7ca
Boris Blondin <blondin.boris@gmx.com>
2024-05-01 08:22:03 +0100

    fixed tests

--- tests/unit/BackupTest.php
+++ tests/unit/BackupTest.php
@@ -8,8 +8,6 @@ class BackupTest extends BaseTest
     {
         $db = \Craft::$app->getDb();
         $db->backupTo(\Craft::getAlias('@storage/db-backups/test.sql'));
-        // $this->saveLogs();
-        // codecept_debug(ActivityLog::find()->all()[1]->type);
         $this->assertLogCount(1);
         $this->assertLatestLog('backupCreated');
     }
